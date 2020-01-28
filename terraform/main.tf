b904ccd6270d5facb97d94b8bf85af4d8667f536
Andrei Yangabishev <waffleboot@gmail.com>
2020-01-28 05:49:07 +0300

    remove unused network from terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,15 +69,6 @@ resource "aws_vpc" "test" {
   }
 }
 
-# resource "aws_vpc" "kubernetes" {
-#   cidr_block           = "172.16.0.0/24"
-#   enable_dns_support   = true
-#   enable_dns_hostnames = false
-#   tags = {
-#     Name = "kubernetes"
-#   }
-# }
-
 resource "aws_subnet" "test" {
   vpc_id                  = aws_vpc.test.id
   cidr_block              = "192.168.0.0/24"
@@ -95,6 +86,59 @@ resource "aws_subnet" "kubernetes" {
   tags = {
     Name               = "kubernetes"
     kubernetes_kubelet = true
+  }
+}
+
+resource "aws_default_security_group" "test" {
+  vpc_id = aws_vpc.test.id
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 6443
+    to_port     = 6443
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_security_group" "kubernetes" {
+  vpc_id = aws_vpc.test.id
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "kubernetes"
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_security_group.kubernetes.id} > kubernetes-security-group"
   }
 }
 
@@ -121,57 +165,11 @@ resource "aws_route_table_association" "test" {
   route_table_id = aws_route_table.test.id
 }
 
-resource "aws_security_group" "test" {
-  vpc_id = aws_vpc.test.id
-  name   = "public"
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-  }
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-  }
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "tcp"
-    from_port   = 6443
-    to_port     = 6443
-  }
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "tcp"
-    from_port   = 7080
-    to_port     = 7080
-  }
-  tags = {
-    Name = "test"
-  }
-  provisioner "local-exec" {
-    command = "echo ${join(",", data.aws_security_groups.default.ids)} > kubernetes-security-group"
-  }
-}
-
-data "aws_security_groups" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [aws_vpc.test.id]
-  }
-  filter {
-    name   = "group-name"
-    values = ["default"]
-  }
-}
-
 resource "aws_instance" "master" {
   ami                    = local.ami
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.test.id
-  vpc_security_group_ids = concat([aws_security_group.test.id], data.aws_security_groups.default.ids)
+  vpc_security_group_ids = [aws_default_security_group.test.id]
   key_name               = "ssh-key"
   iam_instance_profile   = aws_iam_instance_profile.network_profile.id
   tags = {
@@ -186,7 +184,7 @@ resource "aws_instance" "worker" {
   ami                    = local.ami
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.test.id
-  vpc_security_group_ids = concat([aws_security_group.test.id], data.aws_security_groups.default.ids)
+  vpc_security_group_ids = [aws_default_security_group.test.id]
   key_name               = "master-to-worker"
   iam_instance_profile   = aws_iam_instance_profile.network_profile.id
   tags = {
