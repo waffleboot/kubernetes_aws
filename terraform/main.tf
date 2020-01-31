e1c2c497a6e0208e396c39178a4f14901d5980ea
Andrei Yangabishev <waffleboot@gmail.com>
2020-01-31 04:55:25 +0300

    plugins.cri.registry.mirrors for http docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ resource "aws_iam_instance_profile" "network_profile" {
 resource "aws_vpc" "test" {
   cidr_block           = "192.168.0.0/16"
   enable_dns_support   = true
-  enable_dns_hostnames = false
+  enable_dns_hostnames = true
   tags = {
     Name = "test"
   }
